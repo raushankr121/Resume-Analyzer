f59ee540817432bc6d520761dbd4903e470b4dff
Raushan Kumar <raushankumar93341@gmail.com>
2026-09-09 13:14:26 +0530

    adding features in dashboard

--- mern_ai/src/component/Dashboard/dashboard.jsx
+++ mern_ai/src/component/Dashboard/dashboard.jsx
@@ -3,8 +3,20 @@ import styles from './dashboard.module.css'
 
 const dashboard = () => {
   return (
-    <div>dashboard</div>
+    <div className={styles.dashboard}>
+        <div className={styles.DashboardLeft}>
+            <div className={styles.DashboardHeaderTitle}> Smart Resume Analyzer</div>
+            <div className={styles.DashboardHeaderLargeTitle}> Resume Mathing Score</div>
+            <div className={styles.DashboardInstructions}>
+                <div className={styles.InstructionTitle}>🔔 Important Instructions: </div>
+                <p>📄 Please Upload the job description before anayzing the score</p>
+                <p>⬆️ Upload the Resume in the pdf format only</p>
+            </div>
+        </div>
+        <div className= {styles.DashboardRight}>
+            Right Dashboard
+        </div>
+    </div>
   )
 }
-
 export default dashboard
